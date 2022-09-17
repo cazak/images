03658859db9a38c122a3b9c7ec890d2b212ca2f2
kjyra <grigorij_valerev@bk.ru>
2022-09-17 12:59:32 +0300

    Add UuidValidator test for Shared model

---
+++ src/Model/Shared/Test/Service/UuidValidatorTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\Shared\Test\Service;
+
+use App\Model\Shared\Service\UuidValidator;
+use Ramsey\Uuid\Uuid;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+final class UuidValidatorTest extends KernelTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->validator = static::getContainer()->get(UuidValidator::class);
+    }
+
+    public function testSuccess(): void
+    {
+        $uuid = Uuid::uuid4()->toString();
+        self::assertTrue($this->validator->validate($uuid));
+    }
+
+    public function testIncorrect(): void
+    {
+        $uuid = 'incorrect-uuid';
+        self::assertFalse($this->validator->validate($uuid));
+    }
+}
