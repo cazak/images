f0eadced7a706cf8d245b0a0521b1caf2f5a1957
kjyra <grigorij_valerev@bk.ru>
2022-09-20 14:33:12 +0300

    Show profile action

--- src/Controller/Users/MeAction.php
+++ src/Controller/Users/MeAction.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller\Users;
 
+use App\Model\Images\Domain\Repository\Author\AuthorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,11 @@ use Symfony\Component\Routing\Annotation\Route;
 final class MeAction extends AbstractController
 {
     #[Route('/me', name: 'app_me')]
-    public function me(): Response
+    public function me(AuthorRepository $authorRepository): Response
     {
         return $this->render('users/me.html.twig', [
             'user' => $this->getUser(),
+            'author' => $authorRepository->findOneBy(['id' => $this->getUser()->getId()->getValue()]),
         ]);
     }
 }
