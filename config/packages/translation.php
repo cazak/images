fb4e0fdcc2fa2a92838fef6458b4c4ad2f43ca44
kjyra <grigorij_valerev@bk.ru>
2022-10-19 12:37:08 +0300

    Add translation

---
+++ config/packages/translation.php
@@ -0,0 +1,11 @@
+<?php
+
+use Symfony\Config\FrameworkConfig;
+
+return static function (FrameworkConfig $framework) {
+    $framework
+        ->defaultLocale('%env(DEFAULT_LOCALE)%')
+        ->translator()
+        ->defaultPath('%kernel.project_dir%/translations')
+    ;
+};
---
+++ translations/messages.ru.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'Newsfeed' => 'Новостная лента',
+    'My page' => 'Моя страница',
+    'Logout' => 'Выход',
+    'Report post' => 'Пожаловаться',
+    'Comments' => 'Комментариев',
+    'Like' => 'Нравится',
+    'Unlike' => 'Не нравится',
+    'Leave a Reply' => 'Оставить комментарий',
+    'Edit profile' => 'Изменить профиль',
+    'Create post' => 'Создать пост',
+    'Delete avatar' => 'Удалить аватар',
+    'Subscriptions' => 'Подписки',
+    'Followers' => 'Подписчики',
+    'Close' => 'Закрыть',
+];
